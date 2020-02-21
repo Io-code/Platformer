9df9f9a63e19e6522547d7f3397134fda84e00cf
Haxeard <lo.accard@gmail.com>
2020-02-21 10:19:48 +0100

    Controller gestion des pentes

---
+++ PlatformerBase/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RayCastController
+{
+    public Vector3 move;
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 velocity = move * Time.deltaTime;
+        transform.Translate(velocity);
+    }
+
+    void MovePassengers(Vector3 velocity)
+    {
+        float DirectionX = Mathf.Sign(velocity.x);
+        float DirectionY = Mathf.Sign(velocity.y);
+    }
+}
